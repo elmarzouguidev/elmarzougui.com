5b0db1aabc298b844de774d9e69d7124ddcfea20
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-11-28 15:51:03 +0100

    add Spatie/Sluggable package

--- app/Models/CMS/Category.php
+++ app/Models/CMS/Category.php
@@ -2,9 +2,14 @@
 
 namespace App\Models\CMS;
 
+use App\Traits\GetModelByKeyName;
+use App\Traits\HasSlug;
+use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use GetModelByKeyName;
+    use UuidGenerator;
+    use HasSlug;
 }
